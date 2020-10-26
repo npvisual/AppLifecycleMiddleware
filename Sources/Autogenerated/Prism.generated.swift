df190b4d281a7f978084485615b22645f9dc36da
Nick <npvisual@users.noreply.github.com>
2020-10-26 08:56:05 -0500

    Making the extension public to see if this will solve the issue seen in : https://github.com/SwiftRex/SwiftRex/issues/87#issuecomment-715469545

--- Sources/Autogenerated/Prism.generated.swift
+++ Sources/Autogenerated/Prism.generated.swift
@@ -2,44 +2,44 @@
 // DO NOT EDIT
 
 // swiftlint:disable all
-extension AppLifecycleAction {
-    public var didEnterBackground: Void? {
+public extension AppLifecycleAction {
+    var didEnterBackground: Void? {
         guard case .didEnterBackground = self else { return nil }
         return ()
     }
 
-    public var isDidEnterBackground: Bool {
+    var isDidEnterBackground: Bool {
         didEnterBackground != nil
     }
 
-    public var willEnterForeground: Void? {
+    var willEnterForeground: Void? {
         guard case .willEnterForeground = self else { return nil }
         return ()
     }
 
-    public var isWillEnterForeground: Bool {
+    var isWillEnterForeground: Bool {
         willEnterForeground != nil
     }
 
-    public var didBecomeActive: Void? {
+    var didBecomeActive: Void? {
         guard case .didBecomeActive = self else { return nil }
         return ()
     }
 
-    public var isDidBecomeActive: Bool {
+    var isDidBecomeActive: Bool {
         didBecomeActive != nil
     }
 
-    public var willBecomeInactive: Void? {
+    var willBecomeInactive: Void? {
         guard case .willBecomeInactive = self else { return nil }
         return ()
     }
 
-    public var isWillBecomeInactive: Bool {
+    var isWillBecomeInactive: Bool {
         willBecomeInactive != nil
     }
 
-    public var didFinishLaunchingWithOption: [UIApplication.LaunchOptionsKey: Any]?? {
+    var didFinishLaunchingWithOption: [UIApplication.LaunchOptionsKey: Any]?? {
         get {
             guard case let .didFinishLaunchingWithOption(associatedValue0) = self else { return nil }
             return (associatedValue0)
@@ -50,11 +50,11 @@ extension AppLifecycleAction {
         }
     }
 
-    public var isDidFinishLaunchingWithOption: Bool {
+    var isDidFinishLaunchingWithOption: Bool {
         didFinishLaunchingWithOption != nil
     }
 
-    public var willFinishLaunchingWithOptions: [UIApplication.LaunchOptionsKey: Any]?? {
+    var willFinishLaunchingWithOptions: [UIApplication.LaunchOptionsKey: Any]?? {
         get {
             guard case let .willFinishLaunchingWithOptions(associatedValue0) = self else { return nil }
             return (associatedValue0)
@@ -65,7 +65,7 @@ extension AppLifecycleAction {
         }
     }
 
-    public var isWillFinishLaunchingWithOptions: Bool {
+    var isWillFinishLaunchingWithOptions: Bool {
         willFinishLaunchingWithOptions != nil
     }
 }

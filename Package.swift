64718f9bf0969ac010760b2ca7244466cc673047
Luiz Rodrigo Martins Barbosa <luiz@developercity.de>
2021-11-14 21:17:40 +0100

    Adopt Middleware Protocol

--- Package.swift
+++ Package.swift
@@ -24,10 +24,7 @@ let package = Package(
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
-        .package(url: "https://github.com/SwiftRex/SwiftRex.git", from: "0.8.0"),
-//        .package(url: "https://github.com/nicklockwood/SwiftFormat", from: "0.46.3"),
-//        .package(url: "https://github.com/Realm/SwiftLint", from: "0.40.3"),
-//        .package(url: "https://github.com/orta/Komondor", from: "1.0.0"),
+        .package(url: "https://github.com/SwiftRex/SwiftRex.git", .branch("develop")),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
@@ -42,20 +39,3 @@ let package = Package(
         ),
     ]
 )
-
-// The settings for the git hooks for our repo
-//#if canImport(PackageConfig)
-//    import PackageConfig
-//
-//    PackageConfiguration([
-//        "komondor": [
-//            // When someone has run `git commit`, first run
-//            // SwiftFormat and the auto-correcter for SwiftLint
-//            "pre-commit": [
-//                "swift run swiftformat .",
-//                "swift run swiftlint autocorrect",
-//            ],
-//        ],
-//    ])
-//        .write()
-//#endif
--- Tests/AppLifecycleMiddlewareTests/AppLifecycleMiddlewareTests.swift
+++ Tests/AppLifecycleMiddlewareTests/AppLifecycleMiddlewareTests.swift
@@ -3,18 +3,6 @@ import XCTest
 
 final class AppLifecycleMiddlewareTests: XCTestCase {
     func testAppLifecycleActions() {
-        let sut = AppLifecycleMiddleware()
-        var getStateCount = 0
-        var dispatchActionCount = 0
-
-        sut.receiveContext(
-            getState: { getStateCount += 1 },
-            output: .init { _, _ in
-                dispatchActionCount += 1
-            }
-        )
-
-        //        XCTAssertEqual(AppLifecycleMiddleware().text, "Hello, World!")
     }
 
     static var allTests = [

2535ec89e7140526462951d57a4ee4f2e232ca1a
Luiz Rodrigo Martins Barbosa <luiz@developercity.de>
2021-11-23 14:25:08 +0100

    Update SwiftRex to use Middleware IO

--- Package.swift
+++ Package.swift
@@ -24,7 +24,7 @@ let package = Package(
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
-        .package(url: "https://github.com/SwiftRex/SwiftRex.git", .branch("develop")),
+        .package(url: "https://github.com/SwiftRex/SwiftRex.git", .upToNextMajor(from: "0.8.8")),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
